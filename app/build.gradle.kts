af901891df31f794f15ee1b9b0c09ea90eb6ce85
Omar Salim <omaroidd@gmail.com>
2026-05-10 23:20:54 +0300

    build: add compose adaptive dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,11 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.window.size)
+
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
