f7d2f2b4ea8f71a1d65b657b287b8a599e6ad101
Omar Salim <omaroidd@gmail.com>
2026-05-10 23:10:57 +0300

    refactor: clean initial template

--- app/src/main/java/com/omaroid/adaptiveui/MainActivity.kt
+++ app/src/main/java/com/omaroid/adaptiveui/MainActivity.kt
@@ -3,45 +3,15 @@ package com.omaroid.adaptiveui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.omaroid.adaptiveui.ui.theme.ComposeNav3AdaptiveUITheme
+
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
-            ComposeNav3AdaptiveUITheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            AdaptiveApp()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeNav3AdaptiveUITheme {
-        Greeting("Android")
     }
 }
