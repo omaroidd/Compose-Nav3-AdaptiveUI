1d9f8d9d9a540b7fde7c4e1f8df23424f2573359
Omar Salim <omaroidd@gmail.com>
2026-05-11 01:08:16 +0300

    chore: update project configuration and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
 
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.navigation.compose)
 
     testImplementation(libs.junit)
